7eb406141821ef1fe32b132a28967e13af88fafc
Matheus Iacono <matheus@iacono.com.br>
2018-02-13 22:14:51 -0300

    criando estado para o carrinho no App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './App.css';
 import Header from './components/Header';
 
-const App = () => (
-  <div className="App">
-    <Header counter={2} openCart={() => {}} />
-  </div>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showCart: false,
+      cart: [],
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header
+          cart={this.state.cart}
+          showCart={this.state.showCart}
+          openCart={() => this.setState({ showCart: !this.state.showCart })}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
